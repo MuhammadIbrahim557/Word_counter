c79bca7a498986ea0e6393d65f0d9e085f5d1f8b
Muhammad Ibraheem <160319187+MuhammadIbrahim557@users.noreply.github.com>
2024-05-08 15:50:33 -0700

    Push this exciting project "Word counter" to Github.

---
+++ main.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let user_answer = await inquirer.prompt([
+    {
+        name: "words",
+        type: "input",
+        message: "please enter your words",
+    },
+]);
+let result = user_answer.words.trim().split(" ").length;
+console.log(`You have total ${result} words.`);
