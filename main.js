2faf190aeb42541bcb06ff5cf6b7d4c5fd2b919a
Muhammad Ibraheem <160319187+MuhammadIbrahim557@users.noreply.github.com>
2024-05-08 16:03:12 -0700

    just compiled js file

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ let user_answer = await inquirer.prompt([
     {
         name: "words",
         type: "input",
-        message: "please enter your words",
+        message: "Please enter your words:",
     },
 ]);
 let result = user_answer.words.trim().split(" ").length;
-console.log(chalk.red(`You have total ${result} words.`));
+console.log(chalk.red(`You have a total of ${result} word$.`));
